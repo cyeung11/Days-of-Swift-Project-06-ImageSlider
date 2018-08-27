1697571cd35a807a465b495ddebf735dabac5bf4
Chris Yeung <cyeung11@gmail.com>
2018-08-27 19:21:16 +0800

    Initial Commit

---
+++ Project 06 ImageSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 06 ImageSlider
+//
+//  Created by Chris on 27/8/2018.
+//  Copyright © 2018 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
